43bda3fae9c84ee6c15abc7ff771ff08714176b6
Nathan Nielson <nathanbn123@gmail.com>
2019-04-26 09:43:04 -0700

    make app component stateful and create functions to manipulate state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,26 +6,48 @@ import Admin from './Admin';
 import { Switch, Route } from 'react-router-dom';
 import Error404 from './Error404';
 
-function App(){
-  return (
-    <div>
-      <Header/>
-      <Switch>
-        <Route exact path='/' component={Home, Booch}/>
-        <Route exact path='/Admin' component={Admin}/>
-        <Route component={Error404}/>
-      </Switch>
+class App extends React.component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: null,
+      brand: null,
+      flavor: null,
+      price: null,
+      pints: 25
+    };
+  }
+
+  sellSomeBooch() {
+    this.state.pints - 1;
+
+  }
 
 
-      <style jsx global>{`
-        body {
-          font-family: sans-serif;
-          box-style: border-box;
-          background-color: #e7e8d9;
-        }
-        `}</style>
-    </div>
-  );
-}
 
+
+
+  render(){
+    return (
+      <div>
+        <Header/>
+        <Switch>
+          <Route exact path='/' component={Home, Booch}/>
+          <Route exact path='/Admin' component={Admin}/>
+          <Route component={Error404}/>
+        </Switch>
+
+
+        <style jsx global>{`
+            body {
+              font-family: sans-serif;
+              box-style: border-box;
+              background-color: #e7e8d9;
+            }
+            `}</style>
+        </div>
+      );
+    }
+  }
 export default App;
